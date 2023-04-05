fb676623dd71ac4dcd918b555675b9d0b3d577c2
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-04-05 10:49:25 +0700

    menghitung pada jarak 40cm [9.74654594594858, 9.738253991923335, 9.763165140033077, 9.729973767990723, 9.77149242813159]

--- perhitunganPercepatan.py
+++ perhitunganPercepatan.py
@@ -1,13 +1,13 @@
 waktu50Cm = [175.2, 145.4, 175.3, 175.3, 175.2]
 waktu60Cm = [197.2, 197.3, 197.3, 197.3, 197.5]
-waktu40cm = [149.4, 149.5, 149.2, 149.6, 149.1]
-waktu30cm = [119.6, 119.9, 119.6, 119.5, 119.7]
+waktu40Cm = [149.4, 149.5, 149.2, 149.6, 149.1]
+waktu30Cm = [119.6, 119.9, 119.6, 119.5, 119.7]
 
 totalData = len(waktu50Cm)
 himpunanPercepatanGravitasi = []
 percepatanGravitasi = 0
 for i in range(totalData):
-    percepatanGravitasi = 0.6/(0.2*waktu60Cm[i]*0.001 + 0.5*waktu60Cm[i]*0.001*waktu60Cm[i]*0.001)
+    percepatanGravitasi = 0.4/(0.2*waktu40Cm[i]*0.001 + 0.5*waktu40Cm[i]*0.001*waktu40Cm[i]*0.001)
     himpunanPercepatanGravitasi.append(percepatanGravitasi)
 
 print(himpunanPercepatanGravitasi)
