6f590acc80311ee5e90e9865be80a8a08c4b3465
Amario Fausta <amarioharlastputra@gmail.com>
2023-05-22 20:46:29 +0700

    menambahkan code untuk std dan rata rata

--- perhitunganPercepatan.py
+++ perhitunganPercepatan.py
@@ -1,7 +1,7 @@
-waktu50Cm = [175.2, 145.4, 175.3, 175.3, 175.2]
-waktu60Cm = [197.2, 197.3, 197.3, 197.3, 197.5]
-waktu40Cm = [149.4, 149.5, 149.2, 149.6, 149.1]
-waktu30Cm = [119.6, 119.9, 119.6, 119.5, 119.7]
+waktu50Cm = [176.6, 176.5, 177.3, 176.7, 177.4]
+waktu60Cm = [200.8, 201, 200.7, 201.3, 201.7]
+waktu40Cm = [147.8, 148.3, 147.8, 148.4, 148.2]
+waktu30Cm = [117.2, 117.2, 117.5, 117.3, 116.9]
 
 
 waktusensor50cm = [149.85 + 8 , 158.17+ 8, 166.5 + 8, 166.5+ 8, 149.85+ 8]
@@ -12,7 +12,7 @@ totalData = len(waktu50Cm)
 himpunanPercepatanGravitasi = []
 percepatanGravitasi = 0
 for i in range(totalData):
-    percepatanGravitasi = 0.4/(0.2*waktusensor40cm[i]*0.001 + 0.5*waktusensor40cm[i]*0.001*waktusensor40cm[i]*0.001)
+    percepatanGravitasi = 0.6/(0.2*waktu60Cm[i]*0.001 + 0.5*waktu60Cm[i]*0.001*waktu60Cm[i]*0.001)
     himpunanPercepatanGravitasi.append(percepatanGravitasi)
 
 print(himpunanPercepatanGravitasi)
